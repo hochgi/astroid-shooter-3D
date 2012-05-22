6b9efd56e592467def2ed122805659714b577c05
Gilad Hoch <Giladhoch@gmail.com>
2012-05-22 13:45:24 +0300

    organized the code a bit. now there is a big class named orientation, which we can get all the required data from it. still, we need to figure out hoe to rotate a vector around an arbitrary axis (which is represented by another vector)

---
+++ src/Vector.java
@@ -0,0 +1,35 @@
+
+public class Vector {
+	double x;
+	double y;
+	double z;
+	public Vector(double x, double y, double z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+	public double getX() {
+		return x;
+	}
+	public void setX(double x) {
+		this.x = x;
+	}
+	public double getY() {
+		return y;
+	}
+	public void setY(double y) {
+		this.y = y;
+	}
+	public double getZ() {
+		return z;
+	}
+	public void setZ(double z) {
+		this.z = z;
+	}
+	public Vector add(Vector v) {
+		return new Vector(x+v.getX(),y+v.getY(),z+v.getZ());
+	}
+	public Vector sub(Vector v) {
+		return new Vector(x-v.getX(),y-v.getY(),z-v.getZ());
+	}
+}
